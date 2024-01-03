17da77d5e4fb13a46f8c4e1436a2de3eaff7ce54
Edwin Cheng <edwincheng09@googlemail.com>
2024-01-03 23:08:08 +0800

    added cities item to side menu

--- challenge-11-worldwise/src/App.jsx
+++ challenge-11-worldwise/src/App.jsx
@@ -6,17 +6,46 @@ import Login from "../src/pages/Login";
 import Pricing from "../src/pages/Pricing";
 import PageNotFound from "../src/pages/PageNotFound";
 import AppLayout from "../src/pages/AppLayout";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const url = "http://localhost:8000/";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${url}cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Error couldn't fetch");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>List of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
--- challenge-11-worldwise/src/components/CityList.jsx
+++ challenge-11-worldwise/src/components/CityList.jsx
@@ -1,7 +1,25 @@
+import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
+import Spinner from "./Spinner.jsx";
+import PropTypes from "prop-types";
 
-function CityList() {
-  return <ul className={styles.cityList}>LIST</ul>;
+// eslint-disable-next-line react/prop-types
+function CityList({ cities, isLoading }) {
+  CityList.proptypes = {
+    cities: PropTypes.array,
+    isLoading: PropTypes.bool,
+  };
+
+  if (isLoading) return <Spinner />;
+
+  return (
+    <ul className={styles.cityList}>
+      {/* eslint-disable-next-line react/prop-types */}
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
